945c225e8ea693c021c204aa05625dd82892ba7a
EricTsui <erictsui@192-168-1-8.tpgi.com.au>
2016-11-06 08:14:38 +1100

    Change implementation from 'UI by coding ' to 'UI by storyboard'

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,27 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let label = UILabel()
-        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
         
-        let button = UIButton()
-        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
     }
 
-    func incrementCount(){
+    @IBAction func incrementCount(){
         self.count += 1
         self.label.text = "\(self.count)"
     }
